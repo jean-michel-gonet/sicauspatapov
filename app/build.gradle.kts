0563c03b26b706ec95c733570044c3f81ed1f44a
Jean-Michel Gonet <jmgonet@yahoo.com>
2024-05-23 15:57:00 +0200

    Added color and fonts from material 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
